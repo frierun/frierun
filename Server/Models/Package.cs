e31750ddf57175f77e3cd56afd2690698072b5ed
Konstantin Babushkin <koka@idwrx.com>
2024-09-27 10:14:25 +0200

    some test packages

--- Server/Models/Package.cs
+++ Server/Models/Package.cs
@@ -1,3 +1,9 @@
 ﻿namespace Frierun.Server.Models;
 
-public record Package(string Name, string ImageName, int Port);
+public record Package(
+    string Name,
+    string ImageName,
+    int Port,
+    IList<string>? Volumes = null,
+    bool RequireDocker = false
+);
--- Server/Services/PackageRegistry.cs
+++ Server/Services/PackageRegistry.cs
@@ -4,10 +4,24 @@ namespace Frierun.Server.Services;
 
 public class PackageRegistry
 {
-    public IList<Package> Packages { get; } = [
-        new("Heimdall", "lscr.io/linuxserver/heimdall:latest", 80),
+    public IList<Package> Packages { get; } =
+    [
+        // https://hub.docker.com/r/linuxserver/heimdall/
+        new("Heimdall", "lscr.io/linuxserver/heimdall:latest", 80, ["/config"]),
+        
+        // https://github.com/bastienwirtz/homer
+        new("Homer", "b4bz/homer:latest", 8080, ["/www/assets"]),
+        
+        // https://homarr.dev/docs/getting-started/installation/
+        new("Homarr", "ghcr.io/ajnart/homarr:latest", 7575, ["/app/data/configs", "/app/public/icons", "/data"], true),
+        
+        // https://dashy.to/docs/quick-start
+        new("Dashy", "lissy93/dashy:latest", 8080, ["/app/user-data"]),
+        
+        // https://docs.portainer.io/start/install-ce/server/docker/linux
+        new("Portainer", "portainer/portainer-ce:latest", 8000, ["/data"], true)
     ];
-    
+
     public Package? Find(string name)
     {
         return Packages.FirstOrDefault(p => p.Name == name);
