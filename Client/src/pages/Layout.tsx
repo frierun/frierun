798d7d41f84d1ed79901e843bc58c571cd641324
Konstantin Babushkin <koka@idwrx.com>
2025-01-27 10:46:06 +0100

    packages

--- Client/src/pages/Layout.tsx
+++ Client/src/pages/Layout.tsx
@@ -1,15 +1,18 @@
-import {PropsWithChildren} from "react";
+import {PropsWithChildren, useContext} from "react";
 import IndicatorStripe from "../components/IndicatorStripe";
+import StateContext from "@/providers/StateContext.tsx";
 
+type Props = {}
+export default function Layout({children}: PropsWithChildren<Props>) {
+    const {ready} = useContext(StateContext);
 
-type Props = {
-    blocked?: boolean;
-}
-export default function Layout({children, blocked = false}:PropsWithChildren<Props>)
-{
     return <>
-    {blocked &&
-        <div className={"z-50 bg-gray opacity-50 absolute top-20 left-0 right-0 bottom-0 flex justify-center font-bold"}>Please wait...</div>}
+        {!ready &&
+            <div
+                className={"z-50 bg-gray opacity-50 absolute top-20 left-0 right-0 bottom-0 flex justify-center font-bold"}>
+                Please wait...
+            </div>
+        }
         <div id={"header"}>
             <div className={"frame flex-shrink-0"}>
                 <img src={'/svg/logo-full.svg'}/>
@@ -18,7 +21,7 @@ export default function Layout({children, blocked = false}:PropsWithChildren<Pro
                 <img src={'/svg/deco.svg'}/>
             </div>
         </div>
-        <IndicatorStripe running={!!blocked}/>
+        <IndicatorStripe running={!ready}/>
         <div className={"frame my-6"}>
             {children}
         </div>
