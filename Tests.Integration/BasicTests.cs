dae58e46a28c4443e20e9603c013ac1cdb430ecd
Konstantin Babushkin <koka@idwrx.com>
2025-02-19 14:57:37 +0100

    install and uninstall all packages

--- Tests.Integration/BasicTests.cs
+++ Tests.Integration/BasicTests.cs
@@ -1,7 +1,4 @@
-﻿using Docker.DotNet;
-using Docker.DotNet.Models;
-using Frierun.Server.Data;
-using Frierun.Server.Services;
+﻿using Frierun.Server.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
@@ -33,48 +30,5 @@ public class BasicTests
         var package = _factory.Services.GetRequiredService<PackageRegistry>().Find("frierun");
 
         Assert.NotNull(package);
-    }
-
-    [Fact]
-    public async Task InstallingUninstalling_Frierun_ShouldCreateDockerContainer()
-    {
-        var package = _factory.Services.GetRequiredService<PackageRegistry>().Find("frierun")!;
-        var executionService = _factory.Services.GetRequiredService<ExecutionService>();
-        var installService = _factory.Services.GetRequiredService<InstallService>();
-        var uninstallService = _factory.Services.GetRequiredService<UninstallService>();
-        var state = _factory.Services.GetRequiredService<State>();
-        var dockerClient = new DockerClientConfiguration().CreateClient();
-
-        // install frierun package
-        var plan = executionService.Create(package);
-        var application = installService.Handle(plan);
-
-        Assert.NotNull(application);
-        Assert.Contains(state.Resources, resource => resource is Application { Package.Name: "frierun" });
-        var parameters = new ContainersListParameters
-        {
-            Filters = new Dictionary<string, IDictionary<string, bool>>
-            {
-                {
-                    "name",
-                    new Dictionary<string, bool>
-                    {
-                        { "frierun", true}
-                    }
-                }
-            }
-        };
-        var containers = await dockerClient.Containers.ListContainersAsync(parameters);
-        Assert.NotNull(containers);
-        Assert.Single(containers);
-        Assert.Equal("running", containers[0].State);
-        
-        // uninstall frierun package
-        uninstallService.Handle(application);
-        
-        Assert.DoesNotContain(state.Resources, resource => resource is Application { Package.Name: "frierun" });
-        containers = await dockerClient.Containers.ListContainersAsync(parameters);
-        Assert.NotNull(containers);
-        Assert.Empty(containers);
     }
 }
