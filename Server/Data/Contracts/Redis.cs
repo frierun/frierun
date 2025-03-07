8a9d9db824efa84a8d1d977d62ff75aad1843df4
Konstantin Babushkin <koka@idwrx.com>
2025-03-07 09:56:40 +0100

    add redis installer move other packages to use it instead of direct container install

---
+++ Server/Data/Contracts/Redis.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Frierun.Server.Data;
+
+public record Redis(
+    string? Name = null,
+    string? NetworkName = null
+) : Contract(Name ?? "")
+{
+    public string NetworkName { get; init; } = NetworkName ?? "";
+    [JsonIgnore] public ContractId<Network> NetworkId => new(NetworkName);
+    
+    public string ContainerName { get; init; } = "redis" + ((Name ?? "") == "" ? "" : $"-{Name}");
+    [JsonIgnore] public ContractId<Container> ContainerId => new(ContainerName);
+}
---
+++ Tests/Installers/Base/RedisInstallerTests.cs
@@ -0,0 +1,26 @@
+﻿using Frierun.Server.Data;
+
+namespace Frierun.Tests.Installers.Base;
+
+public class RedisInstallerTests : BaseTests
+{
+   
+    [Fact]
+    public void Install_PackageWithContract_CreatesDatabase()
+    {
+        var package = Factory<Package>().Generate() with
+        {
+            Contracts =
+            [
+                new Redis(),
+            ]
+        };
+
+        var application = InstallPackage(package);
+
+        Assert.NotNull(application);
+        var database = application.DependsOn.OfType<RedisDatabase>().First();
+        var dbContainer = database.DependsOn.OfType<DockerContainer>().First();
+        Assert.Equal(database.Host, dbContainer.Name);
+    }    
+}
