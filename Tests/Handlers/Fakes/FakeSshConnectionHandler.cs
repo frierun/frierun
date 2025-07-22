e6f2039159060092886a7cc43ed3ceb9f9d43800
Konstantin Babushkin <koka@idwrx.com>
2025-07-22 22:28:53 +0200

    udocker container tests

--- Tests/Handlers/Fakes/FakeSshConnectionHandler.cs
+++ Tests/Handlers/Fakes/FakeSshConnectionHandler.cs
@@ -1,14 +1,16 @@
-﻿using Frierun.Server.Data;
+﻿using System.Text;
+using Frierun.Server.Data;
 using Frierun.Server.Handlers;
+using NSubstitute;
 using Renci.SshNet;
 
 namespace Frierun.Tests.Handlers;
 
 public class FakeSshConnectionHandler : Handler<SshConnection>, ISshConnectionHandler
 {
-    public ISshClient SshClient { get; } = NSubstitute.Substitute.For<ISshClient>();
+    public ISshClient SshClient { get; } = CreateSshClientSubstitute();
     public ISftpClient SftpClient { get; } = NSubstitute.Substitute.For<ISftpClient>();
-    
+
     public ISshClient CreateSshClient(SshConnection contract)
     {
         return SshClient;
@@ -17,5 +19,26 @@ public class FakeSshConnectionHandler : Handler<SshConnection>, ISshConnectionHa
     public ISftpClient CreateSftpClient(SshConnection contract)
     {
         return SftpClient;
+    }
+
+    private static ISshClient CreateSshClientSubstitute()
+    {
+        var result = NSubstitute.Substitute.For<ISshClient>();
+
+        var iSession = typeof(SshCommand).Assembly.GetType("Renci.SshNet.ISession");
+        if (iSession == null)
+        {
+            throw new Exception("Could not find the ISession interface");
+        }
+
+        result.RunCommand(Arg.Any<string>()).ReturnsForAnyArgs(info =>
+            NSubstitute.Substitute.For<SshCommand>(
+                NSubstitute.Substitute.For([iSession], []),
+                info.Arg<string>(),
+                Encoding.UTF8
+            )
+        );
+
+        return result;
     }
 }
--- Tests/Handlers/Udocker/ContainerHandlerTests.cs
+++ Tests/Handlers/Udocker/ContainerHandlerTests.cs
@@ -5,15 +5,97 @@ namespace Frierun.Tests.Handlers.Udocker;
 
 public class ContainerHandlerTests : BaseTests
 {
+    private readonly Application _udocker;
+
+    public ContainerHandlerTests()
+    {
+        _udocker = InstallPackage("termux-udocker");
+    }
+
     [Fact]
     public void Initialize_ContractWithMountDockerSocket_ReturnsEmpty()
     {
-        var udocker = InstallPackage("termux-udocker");
         var container = Factory<Container>().Generate() with { MountDockerSocket = true };
-        var handler = Handler<ContainerHandler>(udocker);
+        var handler = Handler<ContainerHandler>(_udocker);
 
         var result = handler.Initialize(container, "");
 
         Assert.Empty(result);
+    }
+
+    [Fact]
+    public void Install_Container_CreatesDaemon()
+    {
+        var container = Factory<Container>().Generate() with { MountDockerSocket = false };
+        var package = Factory<Package>().Generate() with { Contracts = [container] };
+
+        var application = InstallPackage(package);
+
+        var daemon = application.Contracts.OfType<Daemon>().Single();
+        Assert.Contains("udocker", daemon.Command);
+        Assert.Contains("--name=" + container.ContainerName, daemon.Command);
+        Assert.Contains(container.ImageName, daemon.Command);
+    }
+
+    [Fact]
+    public void Install_ContainerWithVolume_CreatesPath()
+    {
+        var container = Factory<Container>().Generate() with
+        {
+            MountDockerSocket = false,
+            Mounts = new Dictionary<string, ContainerMount> { { "/test", new ContainerMount() } }
+        };
+        var package = Factory<Package>().Generate() with { Contracts = [container] };
+
+        var application = InstallPackage(package);
+
+        var volume = application.Contracts.OfType<Volume>().Single();
+        var daemon = application.Contracts.OfType<Daemon>().Single();
+        Assert.Contains($"--volume={volume.LocalPath}:/test", daemon.Command);
+
+        var preCommand = daemon.PreCommands.Single(command => command.Contains("mkdir"));
+        Assert.Contains(volume.LocalPath, preCommand);
+    }
+
+    [Fact]
+    public void Install_ContainerWithPort_PublishesPort()
+    {
+        var container = Factory<Container>().Generate() with
+        {
+            MountDockerSocket = false,
+        };
+        var package = Factory<Package>().Generate() with
+        {
+            Contracts =
+            [
+                container,
+                new PortEndpoint(Protocol.Tcp, 80, Container: new ContractId<Container>(container.Name))
+            ]
+        };
+
+        var application = InstallPackage(package);
+
+        var portEndpoint = application.Contracts.OfType<PortEndpoint>().Single();
+        var daemon = application.Contracts.OfType<Daemon>().Single();
+        Assert.Contains($"--publish={portEndpoint.ExternalPort}:80", daemon.Command);
+    }
+
+    [Fact]
+    public void Install_ContainerWithEnv_PassesEnv()
+    {
+        var container = Factory<Container>().Generate() with
+        {
+            MountDockerSocket = false,
+            Env = new Dictionary<string, string>
+            {
+                {"name", "value"}
+            }
+        };
+        var package = Factory<Package>().Generate() with { Contracts = [container] };
+
+        var application = InstallPackage(package);
+
+        var daemon = application.Contracts.OfType<Daemon>().Single();
+        Assert.Contains($"--env=\"name=value\"", daemon.Command);
     }
 }
