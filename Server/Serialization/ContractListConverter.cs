0a07b503b19c3b8c3a22fc81b6df1394fa972bbc
Konstantin Babushkin <koka@idwrx.com>
2025-09-10 22:13:14 +0200

    infer Contract type from ContractId

--- Server/Serialization/ContractListConverter.cs
+++ Server/Serialization/ContractListConverter.cs
@@ -6,6 +6,24 @@ namespace Frierun.Server;
 
 public class ContractListConverter : JsonConverter<ContractList>
 {
+    private delegate Contract? ReadDelegate(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    );
+
+    /// <summary>
+    /// Create delegate for reading a contract of a given type.
+    /// </summary>
+    private ReadDelegate GetDelegateForType(Type contractType, JsonSerializerOptions options)
+    {
+        var contractConverter = options.GetConverter(contractType);
+        return typeof(JsonConverter<>)
+            .MakeGenericType(contractType)
+            .GetMethod("Read")!
+            .CreateDelegate<ReadDelegate>(contractConverter);
+    }
+
     public override ContractList? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType == JsonTokenType.StartArray)
@@ -23,14 +41,31 @@ public class ContractListConverter : JsonConverter<ContractList>
             );
         }
 
-        var dictionaryConverter = (JsonConverter<IReadOnlyDictionary<ContractId, Contract>>)
-            options.GetConverter(typeof(IReadOnlyDictionary<ContractId, Contract>));
-        var dictionary = dictionaryConverter.Read(
-            ref reader, typeof(IReadOnlyDictionary<ContractId, Contract>), options
-        );
-        if (dictionary is null)
+        if (reader.TokenType != JsonTokenType.StartObject)
         {
-            return null;
+            throw new JsonException();
+        }
+
+        var contractIdConverter = (JsonConverter<ContractId>)options.GetConverter(typeof(ContractId));
+        var dictionary = new Dictionary<ContractId, Contract>();
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+            {
+                break;
+            }
+
+            if (reader.TokenType != JsonTokenType.PropertyName)
+            {
+                throw new JsonException();
+            }
+
+            var contractId = contractIdConverter.ReadAsPropertyName(ref reader, typeof(ContractId), options);
+            reader.Read();
+            
+            var contractType = ContractRegistry.GetContractType(contractId.TypeName);
+            var contract = GetDelegateForType(contractType, options)(ref reader, contractType, options);
+            dictionary[contractId] = contract ?? throw new JsonException();
         }
 
         return new ContractList(dictionary);
