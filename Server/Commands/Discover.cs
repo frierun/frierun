495331cfd0b9c9095c80934b5b99fd53926a0831
Konstantin Babushkin <koka@idwrx.com>
2025-10-22 08:58:32 +0200

    install docker application if dockerApiConnection is found

--- Server/Commands/Discover.cs
+++ Server/Commands/Discover.cs
@@ -2,22 +2,11 @@
 
 namespace Frierun.Server;
 
-public class Discover(ILogger<Discover> logger, HandlerRegistry handlerRegistry, State state, StateSerializer stateSerializer)
+public class Discover(DiscoverService discoverService)
     : BaseCommand("discover", "Discover and add contracts.")
 {
     protected override void Execute()
     {
-        logger.LogInformation("Discover and add contracts.");
-
-        foreach (var handler in handlerRegistry.GetAllHandlers())
-        {
-            foreach (var contract in handler.Discover())
-            {
-                logger.LogInformation("Found contract: {type}", contract);
-                state.AddContract(contract);
-            }
-        }
-        
-        stateSerializer.Save(state);
+        discoverService.Discover();
     }
 }
---
+++ Server/Services/DiscoverService.cs
@@ -0,0 +1,60 @@
+﻿using Frierun.Server.Data;
+
+namespace Frierun.Server;
+
+public class DiscoverService(
+    ILogger<Discover> logger,
+    HandlerRegistry handlerRegistry,
+    State state,
+    StateSerializer stateSerializer,
+    PackageRegistry packageRegistry,
+    ExecutionService executionService,
+    InstallService installService
+    )
+{
+    public void Discover()
+    {
+        logger.LogInformation("Discover and add contracts.");
+
+        foreach (var handler in handlerRegistry.GetAllHandlers())
+        {
+            foreach (var contract in handler.Discover())
+            {
+                logger.LogInformation("Found contract: {type}", contract);
+                state.AddContract(contract);
+            }
+        }
+
+        stateSerializer.Save(state);
+        
+        InstallDocker();
+    }
+
+    /// <summary>
+    /// Auto-install docker package if isn't installed and found on the system.
+    /// </summary>
+    private void InstallDocker()
+    {
+        if (state.Applications.Any(app => app.Package?.Name == "docker"))
+        {
+            return;
+        }
+        
+        var dockerApiConnection = state.GetContracts<DockerApiConnection>().FirstOrDefault();
+        if (dockerApiConnection == null)
+        {
+            return;
+        }
+
+        var package = packageRegistry.Find("docker");
+        if (package == null)
+        {
+            logger.LogError("Docker package not found.");
+            return;
+        }
+
+        logger.LogInformation("Installing docker package.");
+        var executionPlan = executionService.Create(package.CreateApplication());
+        installService.Handle(executionPlan);
+    }
+}
